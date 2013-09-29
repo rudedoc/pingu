77c557b07715091a32e3555a4b6f94baf85765bc
rudedoc <finlay.mark@gmail.com>
2013-09-29 23:33:58 +0100

    remove old specs

--- spec/loading_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-require 'yaml'
-require 'ostruct'
-
-data = YAML.load_file('config.yml')['routers']
-
-routers = data.collect { |router| OpenStruct.new(router[1]) }
-
-Pingu::RoutersClient.new(routers).call_routers
